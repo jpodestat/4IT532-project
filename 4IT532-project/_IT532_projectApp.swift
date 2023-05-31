c05befa53aef90ded1f87477dabcc5a36ae6cd9c
Jan Podestát <podj00@vse.cz>
2023-05-31 20:48:05 +0200

    Initial Commit

---
+++ 4IT532-project/_IT532_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _IT532_projectApp.swift
+//  4IT532-project
+//
+//  Created by Jan Podestát on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct _IT532_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
